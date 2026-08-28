3010e4cda55660d0eba65785523dc99e6ae80fdb
LethalOrca <37668670+awan111@users.noreply.github.com>
2026-08-28 14:00:30 +0500

    Set up Express server in server.js
    Removed JSON rewrite configuration and added Express server setup.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-{
-  "rewrites": [
-    {
-      "source": "/(.*)",
-      "destination": "/server.js"
-    }
-  ]
-}
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Aapke API routes yahan aayenge
+app.get('/', (req, res) => {
+  res.send('LethalOrca Backend is Live!');
+});
+
+module.exports = app;
